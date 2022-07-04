3ac541e40409d9b18ac4c5ae7defc03c0d6896c0
Eric Gomes <eric.n.gomes@hotmail.com>
2022-07-04 20:50:42 +0100

    Separated JS Code

---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+	
+            $(document).ready(function() {
+                $("a").on('click', function(event) {
+
+                    if (this.hash !== "") {
+                        event.preventDefault();
+
+                        var hash = this.hash;
+
+                        $('html, body').animate({
+                            scrollTop: $(hash).offset().top
+                        }, 900, function() {
+
+                            window.location.hash = hash;
+                        });
+                    }
+                });
+            });
+	
+
+	
+	$(function() {
+
+$( document ).tooltip({
+
+	items: "[data-tooltip]",
+	content: function() {
+
+		var element = $( this );
+		var text = element.text();
+		var url = element.attr('data-tooltip');
+		return "<img class='map' alt='" + text +
+			"' src='" + url + "'>";
+
+	}
+});
+});
+	
